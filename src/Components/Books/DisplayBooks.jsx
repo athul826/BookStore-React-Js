45ed6a66e83981f4662a62a664a89a9889f95723
athul_tharol <athultharol1994@gmail.com>
2022-09-25 21:26:22 +0530

    Added: book orders api and pagination

--- src/Components/Books/DisplayBooks.jsx
+++ src/Components/Books/DisplayBooks.jsx
@@ -4,78 +4,66 @@ import "./DisplayBooks.css";
 import imageone from "../../Images/imageone.png";
 import BookService from "../../Services/BookService";
 import { useNavigate } from "react-router-dom";
-import GetBookId from "../GetBookId/GetBookId";
 
 const bookservice = new BookService();
 
 function DisplayBooks(props) {
-
   const navigate = useNavigate();
-  const [bookData,setBookData] = useState([])
-   const [view, SetView] = useState(false);
-  
-  const getBookById = (props) => {
-    console.log("props")
-    let data = {
-      "id":props.arrayBook.id
-    }
+  const [bookData, setBookData] = useState([]);
+  const [view, SetView] = useState(false);
 
-    console.log("calling id");
-               console.log(data);
-               bookservice.getBookById(data).then((response) => {
-                    
-      console.log(response);
+  // const getBookById = (props) => {
+  //   console.log("props")
+  //   let data = {
+  //     "id":props.arrayBook.id
+  //   }
 
-          // navigate('/getBookById')
-          setBookData(response.data.book)
-          SetView(true);
-        }).catch((error) =>{
-          console.log(error);
-        })
-  }
+  //   console.log("calling id");
+  //              console.log(data);
+  //              bookservice.getBookById(data).then((response) => {
+
+  //     console.log(response);
+
+  //         // navigate('/getBookById')
+  //         setBookData(response.data.book)
+  //         SetView(true);
+  //       }).catch((error) =>{
+  //         console.log(error);
+  //       })
+
+  const bookDetail = (data) => {
+    // props.listenToBooks(true);
+    props.listenToEachBook(data);
+  };
 
   return (
-    <>
-    {view == false ?
-      
-      <div className="align_books">
-        <div className="book_box" onClick={() => getBookById(props)}>
-          <div>
-            <div className="book_part">
-              <div className="booksImage">
-                <img src={imageone} alt="" className="book-image" />
-              </div>
+    <div className="align_books">
+      <div className="book_box" onClick={() => bookDetail(props.arrayBook)}>
+        <div>
+          <div className="book_part">
+            <div className="booksImage">
+              <img src={imageone} alt="" className="book-image" />
             </div>
-            <div className="Bottom_part">
-              <div className="insidebottompart">
-              <div className="book_text" > {props.arrayBook.name}</div>
+          </div>
+          <div className="Bottom_part">
+            <div className="insidebottompart">
+              <div className="book_text"> {props.arrayBook.name}</div>
               <div className="Author_part">{props.arrayBook.author}</div>
               <div>
                 <span className="rating_part">
                   {" "}
                   4.5
-                  
                   <span className="number_part">(20)</span>
-                  
                 </span>
                 <div className="booksPrice">Rs.{props.arrayBook.price}</div>
-                 <span className='quantity'>({props.arrayBook.quantity})</span> 
-
+                {/* <span className="quantity">({props.arrayBook.quantity})</span> */}
               </div>
             </div>
           </div>
-          
         </div>
       </div>
-      </div>
-
-      
-        :<GetBookId bookdata = {bookData}></GetBookId>
-     }
-      
-      </>
+    </div>
   );
 }
-
 
 export default DisplayBooks;
--- src/Pages/DashBoard/DashBoard.jsx
+++ src/Pages/DashBoard/DashBoard.jsx
@@ -1,58 +1,105 @@
-import React, { useEffect, useState } from 'react';
-import BookService from '../../Services/BookService';
+import React, { useEffect, useState } from "react";
+import BookService from "../../Services/BookService";
 import "./DashBoard.css";
-import DisplayBooks from '../../Components/Books/DisplayBooks';
-import Header from '../../Components/Header/Header';
-
+import DisplayBooks from "../../Components/Books/DisplayBooks";
+import Header from "../../Components/Header/Header";
+import GetBookId from "../../Components/GetBookId/GetBookId";
+import { Pagination } from "@mui/material";
+import usePagination from "../../Components/Pagination/Pagination";
 
 const bookservice = new BookService();
 function DashBoard(props) {
+  const [view, setView] = useState(true);
+  const [bookdata, setBookdata] = useState("");
+  const [bookArray, setBookArray] = useState([]);
 
-    const [bookArray, setBookArray] = useState([]);
+  const [page, setpage] = useState(1);
+  //   const [search, setSearch] = useState("");
 
-    useEffect(() => {
-        getBooks();
-    }, [])
+  useEffect(() => {
+    getBooks();
+  }, []);
 
-    const getBooks = () => {
-        bookservice.getAllBooks() .then((response) => {
-            console.log(response);
-            setBookArray(response.data.books)
-        }).catch((err) => {
-            console.log(err);
-        })
+  const getBooks = () => {
+    setView(true);
+    bookservice
+      .getAllBooks()
+      .then((response) => {
+        console.log(response);
+        setBookArray(response.data.books);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    }
+  const PER_PAGE = 8;
 
-    return (
-         <>
+  var bookArrayLength = bookArray ? bookArray.length : 0;
+  const pageCount = Math.ceil(bookArrayLength / PER_PAGE);
+  const paginate = usePagination(bookArray, PER_PAGE);
 
-            < Header />
+  const changePage = (event, page) => {
+    setpage(page);
+    paginate.jump(page);
+  };
 
+  const listenToEachBook = (data) => {
+    console.log("Listen to Book is calling..");
+    setView(false);
+    console.log(data);
+    setBookdata(data);
+  };
 
-            <div className='topics'>
-                <span className='heading'>Books({bookArray.length})</span>
+  return (
+    <>
+      <Header />
 
-                <select className='arrange'>
-                    <option>Sort by relevance</option>
-                    <option>Price:Low to High</option>
-                    <option>Price:High to Low</option>
-                    <option>Newest Arrivals</option>
-                </select>
+      {view ? (
+        <div>
+          <div className="topics">
+            <span className="heading">Books({bookArray.length})</span>
+
+            <select className="arrange">
+              <option>Sort by relevance</option>
+              <option>Price:Low to High</option>
+              <option>Price:High to Low</option>
+              <option>Newest Arrivals</option>
+            </select>
+          </div>
+
+          <div className="bookscontainer">
+            <div className="getbooks">
+              {paginate.currentData()
+                ? paginate
+                    .currentData()
+                    .map((book, index) => (
+                      <DisplayBooks
+                        key={index}
+                        arrayBook={book}
+                        getBooks={getBooks}
+                        listenToEachBook={listenToEachBook}
+                      />
+                    ))
+                : "no book available"}
             </div>
+          </div>
+        </div>
+      ) : (
+        <GetBookId bookdata={bookdata} />
+      )}
 
-            <div className='bookscontainer' >
-             
-                <div className='getbooks' >
-                {bookArray.length > 0 && bookArray.map((book, index) => (
-                    <DisplayBooks key={index} arrayBook={book} getBooks={getBooks} />
-                    ))}
-
-                    </div>
-                </div>
-    
-            </>
-        );
-    }
+      <div className="pagination">
+        <Pagination
+          count={pageCount}
+          page={page}
+          onChange={changePage}
+          variant="outlined"
+          shape="rounded"
+        />
+      </div>
+    </>
+  );
+}
 
 export default DashBoard;
